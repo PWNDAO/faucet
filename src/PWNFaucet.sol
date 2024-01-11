38203cab84b68b4cb0fa2f57568c18bf55214d42
microHoffman <skozak@protonmail.com>
2024-01-11 15:33:54 +0700

    feat: add second erc20 faucet token, rename ERC721 and ERC1155 faucet tokens

--- src/PWNFaucet.sol
+++ src/PWNFaucet.sol
@@ -9,26 +9,29 @@ import {PWNFaucet1155} from "./PWNFaucet1155.sol";
 contract PWNFaucet {
 
     PWNFaucet20 public t20;
+    PWNFaucet20 public t20S; // (!not real coin, for testing purposes only!) "testing 'stablecoin' erc20"
     PWNFaucet721 public t721;
     PWNFaucet1155 public t1155;
 
 
     constructor() {
-        t20 = new PWNFaucet20("PWN Faucet Token", "PWNf20");
-        t721 = new PWNFaucet721("PWN Faucet NFT", "PWNf721");
+        t20 = new PWNFaucet20("PWN Faucet Token", "PWND");
+        t20S = new PWNFaucet20("PWN S Faucet Token", "PWNS");
+        t721 = new PWNFaucet721("PWN Faucet ERC721", "PWNf721");
         t1155 = new PWNFaucet1155();
     }
 
 
-    /// @dev Drip 100 fungible, 2 non-fungible, and 10 semi-fungible token to the receiver.
+    /// @dev Drip 100 PWND (ERC20), 100 PWNS (ERC20), 3 NFTs (ERC721), and 10 semi-fungible tokens (ERC1155) to the receiver.
     /// @param receiver The address of the receiver.
     function drip(address receiver) external {
         t20.mint(receiver, 100e18);
+        t20S.mint(receiver, 100e18);
 
+        t721.mint(receiver);
         t721.mint(receiver);
         t721.mint(receiver);
 
         t1155.mint(receiver, 10);
     }
-
 }
