6929ed284d804d4b531c970e94b8b0829ae8f515
microHoffman <skozak@protonmail.com>
2024-01-11 16:10:27 +0700

    refactor: small renaming and delete unnecessary comment after CR

--- script/PWNFaucet.s.sol
+++ script/PWNFaucet.s.sol
@@ -21,7 +21,7 @@ contract Deploy is Script {
         PWNFaucet faucet = new PWNFaucet();
         console2.log("PWNFaucet deployed at:", address(faucet));
 
-        console2.log("PWND ERC20 deployed at:", address(faucet.t20()));
+        console2.log("PWND ERC20 deployed at:", address(faucet.t20D()));
         console2.log("PWNS ERC20 deployed at:", address(faucet.t20S()));
         console2.log("PWN Faucet ERC721 deployed at:", address(faucet.t721()));
         console2.log("PWN Faucet ERC1155 deployed at:", address(faucet.t1155()));
--- src/PWNFaucet.sol
+++ src/PWNFaucet.sol
@@ -8,14 +8,14 @@ import {PWNFaucet1155} from "./PWNFaucet1155.sol";
 
 contract PWNFaucet {
 
-    PWNFaucet20 public t20;
-    PWNFaucet20 public t20S; // (!not real coin, for testing purposes only!) "testing 'stablecoin' erc20"
+    PWNFaucet20 public t20D;
+    PWNFaucet20 public t20S;
     PWNFaucet721 public t721;
     PWNFaucet1155 public t1155;
 
 
     constructor() {
-        t20 = new PWNFaucet20("PWN Faucet Token", "PWND");
+        t20D = new PWNFaucet20("PWN D Faucet Token", "PWND");
         t20S = new PWNFaucet20("PWN S Faucet Token", "PWNS");
         t721 = new PWNFaucet721("PWN Faucet ERC721", "PWNf721");
         t1155 = new PWNFaucet1155();
@@ -25,7 +25,7 @@ contract PWNFaucet {
     /// @dev Drip 100 PWND (ERC20), 100 PWNS (ERC20), 3 NFTs (ERC721), and 10 semi-fungible tokens (ERC1155) to the receiver.
     /// @param receiver The address of the receiver.
     function drip(address receiver) external {
-        t20.mint(receiver, 100e18);
+        t20D.mint(receiver, 100e18);
         t20S.mint(receiver, 100e18);
 
         t721.mint(receiver);
